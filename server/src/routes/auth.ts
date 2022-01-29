332df5cb477046ac854623b1d00f748fd46ad698
iptahaha <95496786+iptahaha@users.noreply.github.com>
2022-01-29 13:17:26 +0200

    Revert "Feature/oop server"

--- server/src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import authController from '../controllers/auth';
-
-const router = express.Router();
-
-router.post('/register', authController.register)
-
-export default router;
